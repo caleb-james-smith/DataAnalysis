0cf2f21a1147627994c662b1bc64a26ff2cc1668
Caleb James Smith <colorado.caleb.smith@gmail.com>
2026-04-04 19:55:57 -0500

    Fix date issue: plot year on x-axis.

--- python/plot.py
+++ python/plot.py
@@ -4,14 +4,12 @@ import matplotlib.dates as mdates
 def makeMultiPlot(data, post_types, plot_dir, plot_name, title, x_label, y_label, x_lim, y_lim, colors):
     fig, ax = plt.subplots(figsize=(12, 6))
 
-    # data.plot()
-
     for post_type in post_types:
-        ax.plot(data[post_type], linestyle='-', label=post_type, color=colors[post_type])
+        ax.plot(data["Date"], data[post_type], linestyle='-', label=post_type, color=colors[post_type])
 
     # Format x-axis to show ticks based on time interval
-    ax.xaxis.set_major_locator(mdates.MonthLocator())
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    ax.xaxis.set_major_locator(mdates.YearLocator())
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
 
     plt.title(title,    fontsize=20)
     plt.xlabel(x_label, fontsize=16)
@@ -22,7 +20,8 @@ def makeMultiPlot(data, post_types, plot_dir, plot_name, title, x_label, y_label
         plt.ylim(y_lim)
     ax.legend()
     plt.grid(True)
-    plt.xticks(rotation=45, fontsize=10)
+    # plt.xticks(rotation=45, fontsize=10)
+    plt.xticks(fontsize=10)
     plt.tight_layout()
 
     savePlot(plot_dir, plot_name)
